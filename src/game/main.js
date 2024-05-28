aeec82fae9070d27b2d190149bd6730c6a9cc138
Daniel Alejandro <d.toro05@ufromail.cl>
2024-05-28 03:46:04 -0400

    Add Enum key assets

--- src/game/main.js
+++ src/game/main.js
@@ -1,24 +1,29 @@
 import { Boot } from './scenes/Boot';
-import { Game } from './scenes/Game';
-import { GameOver } from './scenes/GameOver';
-import { MainMenu } from './scenes/MainMenu';
-import Phaser from 'phaser';
+
+import { FightScene } from './scenes/FightScene';
 import { Preloader } from './scenes/Preloader';
+import Phaser from 'phaser';
+
+
 
 // Find out more information about the Game Config at:
 // https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config = {
-    type: Phaser.AUTO,
-    width: 1024,
-    height: 768,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
+    type: Phaser.CANVAS,
+    pixelArt: false,
+    
+    backgroundColor: 0xffffff,
+    scale:{
+        parent: 'game-container',
+        width: 1300,
+        heigth: 630,
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
     scene: [
-        Boot,
         Preloader,
-        MainMenu,
-        Game,
-        GameOver
+        FightScene
+
     ],
     physics: {
         default: 'arcade',
--- src/game/scenes/FightScene.js
+++ src/game/scenes/FightScene.js
@@ -3,7 +3,7 @@ import { Scene } from 'phaser';
 export class FightScene extends Scene{
 
     constructor(){
-        super()
+        super('FightScene')
     }
 
     preload(){
@@ -12,11 +12,7 @@ export class FightScene extends Scene{
 
     create(){
         this.add.text(500,100,"hola")
-        this.add.rectangle(100,100,100,100,0xffffff,1).setInteractive().on('pointerdown', (()=>{
-            this.scene.run("Preloader")
-            
-        }))
-       
+        this.add.rectangle(100,100,100,100,0xffffff,1)
     }
 
 
