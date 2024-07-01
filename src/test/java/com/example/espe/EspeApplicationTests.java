a7375262f89f6da120d09bfcfe3e1f6f80fb5494
Felix216 <216felix.santana@gmail.com>
2024-07-01 13:00:34 -0400

    Instalacion dependencias
    Instalacion SpringBoot

---
+++ src/test/java/com/example/espe/EspeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.espe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
